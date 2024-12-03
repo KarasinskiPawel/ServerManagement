428b464f559dbc1f7c00912200c7ec81b769b4ea
Karasiński Paweł <pawelkarasinski1981@o2.pl>
2024-12-03 10:57:15 +0100

    2024-12-03-Ready

--- obj/Debug/net8.0/ServerManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/ServerManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f238dcc5cddbf3db1e5cdc6d3003e208ec35cb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
